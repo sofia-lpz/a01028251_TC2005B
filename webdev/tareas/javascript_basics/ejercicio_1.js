86f1ecdab443d9b1a07e76318e0f5774c7379110
sofia-lpz <a01028251@tec.mx>
2024-04-08 21:11:02 -0600

    started node project and first 3 exercises

---
+++ webdev/tareas/javascript_basics/ejercicio_1.js
@@ -0,0 +1,5 @@
+function primerchar (cadena) {
+    return cadena.charAt(0);
+}
+
+console.log(primerchar("abacddbec"));
---
+++ webdev/tareas/javascript_basics/ejercicio_3.js
@@ -0,0 +1,6 @@
+function reverse(arreglo) {
+    let reverseArr = [];
+    for (i = 0; i < length(arreglo); i++){
+
+    }
+}
