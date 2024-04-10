82f0cc4345e34e5b2a091e709842acbd0f7d371b
sofia-lpz <a01028251@tec.mx>
2024-04-10 11:39:34 -0600

    finished exercise 8

---
+++ webdev/tareas/javascript_basics/Ejercicio_8.js
@@ -0,0 +1,19 @@
+/*
+Escribe una función que quite los elementos duplicados
+de un arreglo y regrese una lista con los elementos que quedan.
+Por ejemplo: quitaDuplicados([1, 0, 1, 1, 0, 0]) -> [1, 0]
+*/
+
+function quitaDuplicados (arr) {
+    let dict = {};
+    let list = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (dict[arr[i]] == undefined) {
+            dict[arr[i]] = true;
+            list.push(arr[i]);
+        }
+    }
+    return list;
+}
+
+console.log(quitaDuplicados([1, 0, 1, 1, 0, 0]));
