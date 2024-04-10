464c18b58f8e2d0b85f072ea7c1f0e53097fe94d
sofia-lpz <a01028251@tec.mx>
2024-04-10 11:20:20 -0600

    finished exercise 7

---
+++ webdev/tareas/javascript_basics/ejercicio_7.js
@@ -0,0 +1,14 @@
+/*Escribe una función que reciba un número, 
+y regrese una lista con todos sus factores. 
+Por ejemplo: factoriza(12) -> [1, 2, 3, 4, 6, 12].
+*/
+
+function factoriza(num){
+    let factors = [];
+    for (i = 1; i <= num; i++){
+        if (num % i == 0){
+            factors.push(i);
+        }
+    }
+    return factors;
+}
