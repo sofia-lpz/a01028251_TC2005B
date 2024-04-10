cd130e26bbc531ef813d3d3bc77de9ac8fd604e3
sofia-lpz <a01028251@tec.mx>
2024-04-10 11:24:39 -0600

    finished 7

--- webdev/tareas/javascript_basics/ejercicio_7.js
+++ webdev/tareas/javascript_basics/ejercicio_7.js
@@ -1,7 +1,7 @@
 /*Escribe una función que reciba un número, 
 y regrese una lista con todos sus factores. 
 Por ejemplo: factoriza(12) -> [1, 2, 3, 4, 6, 12].
-<<<<<<< HEAD
+<<<<<<< HEA
 
 Sofia Moreno Lopez
 =======
