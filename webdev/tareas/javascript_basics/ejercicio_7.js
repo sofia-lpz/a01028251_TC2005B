e89c9486dcd100bfc4e486ea918a91f7876d0968
Sofia Moreno <a01028251@tec.mx>
2024-04-10 11:25:34 -0600

    Update ejercicio_7.js

--- webdev/tareas/javascript_basics/ejercicio_7.js
+++ webdev/tareas/javascript_basics/ejercicio_7.js
@@ -1,11 +1,8 @@
 /*Escribe una función que reciba un número, 
 y regrese una lista con todos sus factores. 
 Por ejemplo: factoriza(12) -> [1, 2, 3, 4, 6, 12].
-<<<<<<< HEA
 
 Sofia Moreno Lopez
-=======
->>>>>>> 464c18b58f8e2d0b85f072ea7c1f0e53097fe94d
 */
 
 function factoriza(num){
