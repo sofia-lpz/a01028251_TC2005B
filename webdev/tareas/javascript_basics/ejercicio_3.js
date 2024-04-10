243de2e2323ca148089a202d8aa00b1d56572a56
sofia-lpz <a01028251@tec.mx>
2024-04-10 11:10:51 -0600

    finished exercise 3

--- webdev/tareas/javascript_basics/ejercicio_3.js
+++ webdev/tareas/javascript_basics/ejercicio_3.js
@@ -1,6 +1,31 @@
-function reverse(arreglo) {
-    let reverseArr = [];
-    for (i = 0; i < length(arreglo); i++){
+/*Escribe dos funciones: la primera que invierta un arreglo de números 
+y regrese un nuevo arreglo con el resultado; 
+la segunda que modifique el mismo arreglo que se pasa como argumento. 
+No se permite usar la función integrada 'reverse'.
 
+Sofia Moreno Lopez
+*/
+
+function reverseNew(array) {
+    let reverseArr = [];
+    for (i = 0; i < array.length; i++){
+        reverseArr.unshift(array[i]);
     }
+    return reverseArr;
 }
+
+function reverseInPlace(array) {
+    for (i = 0; i < array.length / 2; i++){
+        let aux = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = aux;
+    }
+    return array;
+}
+
+let test = [1, 2, 3, 4, 5];
+console.log("reverseNew: ", reverseNew(test)); 
+console.log("test array after reverseNew: ", test);
+
+console.log("reverseInPlace: ", reverseInPlace(test));
+console.log("test array after reverseInPlace: ", test);
