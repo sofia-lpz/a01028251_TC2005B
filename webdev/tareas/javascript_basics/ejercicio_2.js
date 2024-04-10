884b286c07dd010c4018cbd0ecc808f4a06275af
sofia-lpz <a01028251@tec.mx>
2024-04-10 10:48:24 -0600

    finished exercise 2

--- webdev/tareas/javascript_basics/ejercicio_2.js
+++ webdev/tareas/javascript_basics/ejercicio_2.js
@@ -1,4 +1,24 @@
-function bubblesort (list) {
+/*
+Escribe una función que implemente el algoritmo 'bubble-sort' 
+para ordenar una lista de números.
 
-    
+Sofia Moreno Lopez
+*/
+
+function bubblesort (list) {
+    let swap;
+    do {
+        swap = false;
+        for (let i = 0; i < list.length - 1; i++) {
+            if (list[i] > list[i+1]) {
+                let aux = list[i];
+                list[i] = list[i+1];
+                list[i+1] = aux;
+                swap = true;
+            }
+        }
+    } while (swap);
+    return list;
 }
+
+console.log(bubblesort([5, 3, 8, 1, 2, 7, 4, 6]));
