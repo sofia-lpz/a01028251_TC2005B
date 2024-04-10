0b3d6763d037c05cd428fb4fca0be1daf904b34e
sofia-lpz <a01028251@tec.mx>
2024-04-10 11:54:48 -0600

    format (finished exercise)

--- webdev/tareas/javascript_basics/Ejercicio_14.js
+++ webdev/tareas/javascript_basics/Ejercicio_14.js
@@ -3,3 +3,13 @@ Escribe una función que tome un número
  y devuelva verdadero si es una potencia de dos, 
  falso de lo contrario.
 */
+
+function isPowerOfTwo(num){
+    if (num == 1){
+        return true;
+    }
+    if (num % 2 == 0){
+        return isPowerOfTwo(num / 2);
+    }
+    return false;
+}
