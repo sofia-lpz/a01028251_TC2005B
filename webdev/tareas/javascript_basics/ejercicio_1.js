2df04cb454b0fe2c081a6b25d01e4f8b17848e5a
sofia-lpz <a01028251@tec.mx>
2024-04-10 09:45:51 -0600

    finished exercise 1

--- webdev/tareas/javascript_basics/ejercicio_1.js
+++ webdev/tareas/javascript_basics/ejercicio_1.js
@@ -1,5 +1,31 @@
-function primerchar (cadena) {
-    return cadena.charAt(0);
+/*
+Escribe una función que encuentre el primer 
+carácter de un string de texto que no se repite. 
+Prueba tu función con: 'abacddbec'
+
+Sofia Moreno Lopez
+A01028251
+*/
+
+function firstNonRepeat (string) {
+    let dict = {};
+    for (let i = 0; i < string.length; i++) {
+        if (dict[string[i]] == 1) {
+            delete dict[string[i]];
+        }
+        else if (dict[string[i]] == undefined) {
+            dict[string[i]] = 1;
+        }
+    }
+
+    for (let i = 0; i < string.length; i++) {
+        if (dict[string[i]] == 1) {
+            return string[i];
+        }
+    }
+    return null;
 }
 
-console.log(primerchar("abacddbec"));
+console.log(firstNonRepeat("abacddbec"));
+
+
