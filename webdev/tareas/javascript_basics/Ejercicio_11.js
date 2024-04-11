3de0ceabff3ce4bdade3a6ff5e004084493ded62
sofia-lpz <a01028251@tec.mx>
2024-04-11 00:45:37 -0600

    finished exercise 11

---
+++ webdev/tareas/javascript_basics/Ejercicio_11.js
@@ -0,0 +1,10 @@
+/*
+Escribe una función que tome una lista de cadena de textos 
+y devuelva una nueva lista con todas las cadenas en orden alfabético.
+*/
+
+function sortStrings(arr) {
+    return arr.sort();
+}
+
+console.log(sortStrings(['bebe', 'camino', 'arbol', 'zorro']));
