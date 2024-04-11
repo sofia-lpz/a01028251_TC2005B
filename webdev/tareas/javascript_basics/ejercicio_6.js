99c1dd952260e8db07306d41e6d4abbd26b65454
sofia-lpz <a01028251@tec.mx>
2024-04-11 00:30:52 -0600

    finished exercise 6

---
+++ webdev/tareas/javascript_basics/ejercicio_6.js
@@ -0,0 +1,27 @@
+/*
+Crea una función que cambie una cadena de texto a 'Hacker Speak'
+Por ejemplo, para la cadena 'Javascript es divertido', 
+su hacker speak es: 'J4v45c1pt 35 d1v3rt1d0'.
+*/
+
+function hackerSpeak (str) {
+    let dict = {
+        a: 4,
+        e: 3,
+        i: 1,
+        o: 0,
+        s: 5,
+        t: 7,
+        g: 6,
+        l: 1,
+        z: 2
+    }
+    let hackerStr = '';
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i].toLowerCase();
+        hackerStr += dict[char] || char;
+    }
+    return hackerStr;
+}
+
+console.log(hackerSpeak('Javascript es divertido'));
